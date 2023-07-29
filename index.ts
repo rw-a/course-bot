d420da99a73977c3d2bb997911c0dc042d300d12
rw-a <rwa2684@gmail.com>
2023-07-29 15:44:47 +1000

    init bot

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
