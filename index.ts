5a9641947d2a2285039012ec164b798a68b58c92
rw-a <rwa2684@gmail.com>
2023-07-30 14:51:44 +1000

    changed to es6 imports

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] }) as CommandCli
 
 // Load commands
 client.commands = new Collection();
-const foldersPath = path.join(__dirname, 'commands');
+const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
