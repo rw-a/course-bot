6286319fa432a3c5af22447feb98067599a30e5b
rw-a <rwa2684@gmail.com>
2023-07-31 15:50:07 +1000

    finished command to show courses

--- src/commands/courses.ts
+++ src/commands/courses.ts
@@ -7,6 +7,22 @@ module.exports = {
 		.setDescription('Gets the list of courses.'),
 	async execute(interaction: CommandInteraction) {
 		const coursesStorage = new CoursesStorage();
-		await interaction.reply(`Here are the courses:\n ${coursesStorage.courses.join("\n")}`);
+
+		// Group the courses by their letter codes
+		let response = "Here are the courses:```";
+		let lastCourseGroup = "";
+		for (const courseCode of coursesStorage.courses) {
+			const courseGroup = courseCode.slice(0, 4);
+			if (courseGroup === lastCourseGroup) {
+				response += `\t${courseCode}`;
+			} else {
+				response += `\n${courseCode}`;
+				lastCourseGroup = courseGroup;
+			}
+		}
+
+		response += "```";
+
+		await interaction.reply(response);
 	},
 };
